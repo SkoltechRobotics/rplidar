cbe236d0bb9f50c3c6859d6789f67dc138de4676
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2017-03-28 13:21:36 +0300

    Updated animate example

--- examples/animate.py
+++ examples/animate.py
@@ -1,38 +1,36 @@
+#!/usr/bin/env python3
 '''Animates distances and measurment quality'''
 from rplidar import RPLidar
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
 PORT_NAME = '/dev/ttyUSB0'
-DMAX = 6000
+DMAX = 4000
 IMIN = 0
 IMAX = 50
 
-
-def update(plot, scan):
-    '''Updates plot'''
+def update_line(num, iterator, line):
+    scan = next(iterator)
     offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
-    plot.set_offsets(offsets)
+    line.set_offsets(offsets)
     intens = np.array([meas[0] for meas in scan])
-    plot.set_array(intens)
-    plt.show()
-    plt.pause(0.001)
-
+    line.set_array(intens)
+    return line,
 
 def run():
-    '''Main function'''
-    plt.ion()
     lidar = RPLidar(PORT_NAME)
-    subplot = plt.subplot(111, projection='polar')
-    plot = subplot.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
+    fig = plt.figure()
+    ax = plt.subplot(111, projection='polar')
+    line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
                            cmap=plt.cm.Greys_r, lw=0)
-    subplot.set_rmax(DMAX)
-    subplot.grid(True)
+    ax.set_rmax(DMAX)
+    ax.grid(True)
+
+    iterator = lidar.iter_scans()
+    ani = animation.FuncAnimation(fig, update_line,
+        fargs=(iterator, line), interval=50)
     plt.show()
-    for scan in lidar.iter_scans():
-        if not plt.get_fignums():
-            break
-        update(plot, scan)
     lidar.stop()
     lidar.disconnect()
 
